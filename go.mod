0cb13a17e2cc02891fbb521c4fa8cda64e7d0cc1
ultram4rine <ultramarine730@gmail.com>
2020-03-03 16:05:37 +0400

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/ultram4rine/logserver
+
+go 1.14
+
+require (
+	github.com/ClickHouse/clickhouse-go v1.3.14
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/golang/protobuf v1.3.4
+	github.com/jmoiron/sqlx v1.2.0
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200302123026-7795fca6ccb1 // indirect
+	google.golang.org/grpc v1.27.1
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
---
+++ logserver.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/ultram4rine/logserver/proto"
+
+	"github.com/BurntSushi/toml"
+	_ "github.com/ClickHouse/clickhouse-go"
+	"github.com/jmoiron/sqlx"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var config struct {
+	Port string `toml:"listen_port"`
+	DB   db     `toml:"db"`
+}
+
+type db struct {
+	Host string `toml:"host"`
+	Name string `toml:"name"`
+	User string `toml:"user"`
+	Pass string `toml:"pass"`
+}
+
+var confpath = kingpin.Flag("conf", "Path to config file.").Short('c').Default("logserver.conf.toml").String()
+
+func main() {
+	kingpin.Parse()
+
+	if _, err := toml.DecodeFile(*confpath, &config); err != nil {
+		log.Fatalf("Error decoding config file from %s", *confpath)
+	}
+
+	conn, err := sqlx.Open("clickhouse", fmt.Sprintf("%s?username=%s&password=%s&database=%s", config.DB.Host, config.DB.User, config.DB.Pass, config.DB.Name))
+	if err != nil {
+		log.Fatalf("Error connecting to database: %s", err)
+	}
+	defer conn.Close()
+
+	listener, err := net.Listen("tcp", ":"+config.Port)
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterLogServer(grpcServer, &server{})
+	grpcServer.Serve(listener)
+}
+
+type server struct{}
+
+func (s *server) GetDHCPLog(c context.Context, request *pb.DHCPLogEntry) (response *pb.DHCPLog, err error) {
+	return nil, nil
+}
+
+func (s *server) GetSwitchLog(c context.Context, request *pb.SwitchLogEntry) (response *pb.SwitchLog, err error) {
+	return nil, nil
+}
