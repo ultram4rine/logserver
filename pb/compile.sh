1fcbf1f1843c94af3e68aa523d72820ffc990695
ultram4rine <ultramarine730@gmail.com>
2020-05-22 22:19:45 +0400

    compile web too

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 protoc -I . log.proto --go_out=plugins=grpc:.
+protoc -I . log.proto --js_out=import_style=commonjs:../web/client/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:../web/client/src/
