1ef93a3a54d0ed7426f97bc2dbe7a89f8c1be5bc
ultram4rine <ultramarine730@gmail.com>
2020-07-02 12:33:57 +0400

    switched to vue cli project structure

---
+++ web/client/ui/vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "../public"),
+  assetsDir: process.env.NODE_ENV === "production" ? "static" : "",
+  devServer: {
+    public: "localhost:8080",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  configureWebpack: {
+    plugins: [new CleanWebpackPlugin()],
+  },
+};
