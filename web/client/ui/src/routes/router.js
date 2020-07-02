b9d990c504480774d4ba3998f20a99e3d1f7dc38
ultram4rine <ultramarine730@gmail.com>
2020-07-02 14:21:25 +0400

    fixed components paths

--- web/client/ui/src/routes/router.js
+++ web/client/ui/src/routes/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "./components/Home";
-import Login from "./components/Login";
+import Home from "../components/Home.vue";
+import Login from "../components/Login.vue";
 
 Vue.use(VueRouter);
 
