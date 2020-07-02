69c1d572579f1ac87329e20d35ed761fed8a29f3
ultram4rine <ultramarine730@gmail.com>
2020-07-02 12:19:02 +0400

    no need in grpc_web

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 protoc -I . log.proto --go_out=plugins=grpc:.
-protoc -I . log.proto --js_out=import_style=commonjs:../web/client/src/ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:../web/client/src/
