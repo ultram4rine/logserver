a51c98de75da011d83af25a821feec7528dca3e5
ultram4rine <ultramarine730@gmail.com>
2020-09-04 13:22:47 +0400

    added swagger

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 protoc -I. --go_out=plugins=grpc,paths=source_relative:. log.proto
 protoc -I. --grpc-gateway_out=logtostderr=true,paths=source_relative:. log.proto
+protoc -I. --swagger_out=logtostderr=true:. log.proto
