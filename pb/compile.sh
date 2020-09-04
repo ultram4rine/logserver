a06eb05ae55bcc068bcf2fd98475b58e71d7ec14
ultram4rine <ultramarine730@gmail.com>
2020-09-04 13:17:34 +0400

    changed compile script

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-protoc -I. --go_out=plugins=grpc:. log.proto 
-protoc -I. --grpc-gateway_out=logtostderr=true:. log.proto
+protoc -I. --go_out=plugins=grpc,paths=source_relative:. log.proto
