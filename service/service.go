4ab9a7921146c847cdbadec0cb55b8313d73f6da
ultram4rine <ultramarine730@gmail.com>
2020-09-04 14:22:41 +0400

    rewrited service

--- service/service.go
+++ service/service.go
@@ -5,14 +5,16 @@ import (
 	"net"
 	"time"
 
+	"git.sgu.ru/ultramarine/logserver/pb"
+
 	"github.com/jmoiron/sqlx"
 )
 
 // Service interface.
 type Service interface {
-	GetDHCPLogs(ctx context.Context, mac string, from, to int64) (DHCPLogsResponse, error)
-	GetSwitchLogs(ctx context.Context, name string, from, to int64) (SwitchLogsResponse, error)
-	GetSimilarSwitches(ctx context.Context, name string) (SimilarSwitchesResponse, error)
+	GetDHCPLogs(ctx context.Context, req *pb.DHCPLogsRequest) (*pb.DHCPLogsResponse, error)
+	GetSwitchLogs(ctx context.Context, req *pb.SwitchLogsRequest) (*pb.SwitchLogsResponse, error)
+	GetSimilarSwitches(ctx context.Context, req *pb.SimilarSwitchesRequest) (*pb.SimilarSwitchesResponse, error)
 }
 
 // LogService is a Service interface implementation.
@@ -21,84 +23,84 @@ type LogService struct {
 }
 
 // GetDHCPLogs returns DHCP logs from given MAC address and time interval.
-func (s LogService) GetDHCPLogs(ctx context.Context, mac string, from, to int64) (DHCPLogsResponse, error) {
-	timeFrom, timeTo := parseTime(from, to)
+func (s LogService) GetDHCPLogs(ctx context.Context, req *pb.DHCPLogsRequest) (*pb.DHCPLogsResponse, error) {
+	timeFrom, timeTo := parseTime(req.From, req.To)
 
-	rows, err := s.DB.QueryxContext(ctx, "SELECT ts, message, ip FROM dhcp.events WHERE mac = MACStringToNum(?) AND ts > ? AND ts < ? ORDER BY ts DESC", mac, timeFrom, timeTo)
+	rows, err := s.DB.QueryxContext(ctx, "SELECT ts, message, ip FROM dhcp.events WHERE mac = MACStringToNum(?) AND ts > ? AND ts < ? ORDER BY ts DESC", req.MAC, timeFrom, timeTo)
 	if err != nil {
-		return DHCPLogsResponse{}, err
+		return &pb.DHCPLogsResponse{}, err
 	}
 
-	var logs DHCPLogsResponse
+	var logs *pb.DHCPLogsResponse
 	for rows.Next() {
 		var (
-			l  dhcpLog
+			l  *pb.DHCPLog
 			ip net.IP
 		)
-		if err = rows.Scan(&l.TimeStamp, &l.Message, &ip); err != nil {
-			return DHCPLogsResponse{}, err
+		if err = rows.Scan(&l.Timestamp, &l.Message, &ip); err != nil {
+			return &pb.DHCPLogsResponse{}, err
 		}
 
-		l.IP = ip.String()
+		l.Ip = ip.String()
 
 		logs.Logs = append(logs.Logs, l)
 	}
 	if rows.Err() != nil {
-		return DHCPLogsResponse{}, err
+		return &pb.DHCPLogsResponse{}, err
 	}
 
 	return logs, nil
 }
 
 // GetSwitchLogs returns logs from given switch and time interval.
-func (s LogService) GetSwitchLogs(ctx context.Context, name string, from, to int64) (SwitchLogsResponse, error) {
-	timeFrom, timeTo := parseTime(from, to)
+func (s LogService) GetSwitchLogs(ctx context.Context, req *pb.SwitchLogsRequest) (*pb.SwitchLogsResponse, error) {
+	timeFrom, timeTo := parseTime(req.From, req.To)
 
-	rows, err := s.DB.QueryxContext(ctx, "SELECT ts_remote, log_msg FROM switchlogs WHERE sw_name = ? AND ts_local > ? AND ts_local < ? ORDER BY ts_local DESC", name, timeFrom, timeTo)
+	rows, err := s.DB.QueryxContext(ctx, "SELECT ts_remote, log_msg FROM switchlogs WHERE sw_name = ? AND ts_local > ? AND ts_local < ? ORDER BY ts_local DESC", req.Name, timeFrom, timeTo)
 	if err != nil {
-		return SwitchLogsResponse{}, err
+		return &pb.SwitchLogsResponse{}, err
 	}
 
-	var logs SwitchLogsResponse
+	var logs *pb.SwitchLogsResponse
 	for rows.Next() {
-		var l switchLog
-		if err = rows.Scan(&l.TimeStamp, &l.Message); err != nil {
-			return SwitchLogsResponse{}, err
+		var l *pb.SwitchLog
+		if err = rows.Scan(&l.Ts, &l.Message); err != nil {
+			return &pb.SwitchLogsResponse{}, err
 		}
 
 		logs.Logs = append(logs.Logs, l)
 	}
 	if rows.Err() != nil {
-		return SwitchLogsResponse{}, err
+		return &pb.SwitchLogsResponse{}, err
 	}
 
 	return logs, nil
 }
 
 // GetSimilarSwitches returns available for view logs switches, which names are similar to given.
-func (s LogService) GetSimilarSwitches(ctx context.Context, name string) (SimilarSwitchesResponse, error) {
-	rows, err := s.DB.QueryxContext(ctx, "SELECT DISTINCT sw_name, sw_ip FROM switchlogs WHERE sw_name LIKE ?", name+"%")
+func (s LogService) GetSimilarSwitches(ctx context.Context, req *pb.SimilarSwitchesRequest) (*pb.SimilarSwitchesResponse, error) {
+	rows, err := s.DB.QueryxContext(ctx, "SELECT DISTINCT sw_name, sw_ip FROM switchlogs WHERE sw_name LIKE ?", req.Name+"%")
 	if err != nil {
-		return SimilarSwitchesResponse{}, err
+		return &pb.SimilarSwitchesResponse{}, err
 	}
 
-	var switches SimilarSwitchesResponse
+	var switches *pb.SimilarSwitchesResponse
 	for rows.Next() {
 		var (
-			s  similarSwitch
+			s  *pb.SimilarSwitch
 			IP net.IP
 		)
 
 		if err = rows.Scan(&s.Name, &IP); err != nil {
-			return SimilarSwitchesResponse{}, err
+			return &pb.SimilarSwitchesResponse{}, err
 		}
 
 		s.IP = IP.String()
 
-		switches.Sws = append(switches.Sws, s)
+		switches.Switches = append(switches.Switches, s)
 	}
 	if rows.Err() != nil {
-		return SimilarSwitchesResponse{}, err
+		return &pb.SimilarSwitchesResponse{}, err
 	}
 
 	return switches, nil
