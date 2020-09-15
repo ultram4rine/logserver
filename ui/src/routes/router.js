cb730fb24b2320a007f16def5fdd70f6454ad50e
ultram4rine <ultramarine730@gmail.com>
2020-09-15 19:29:01 +0400

    added auth checking before each route

--- ui/src/routes/router.js
+++ ui/src/routes/router.js
@@ -1,15 +1,46 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../components/Home.vue";
-import Login from "../components/Login.vue";
+import Home from "@/components/Home.vue";
+import Login from "@/components/Login.vue";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: "history",
   routes: [
-    { path: "/", component: Home, meta: { requiresAuth: true } },
-    { path: "/login", component: Login },
+    {
+      path: "/login",
+      component: Login,
+      meta: { skipIfAuth: true },
+    },
+    {
+      path: "/",
+      component: Home,
+      meta: { requiresAuth: true },
+    },
   ],
 });
+
+router.beforeEach((to, _from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!store.getters["auth/isAuthenticated"]) {
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else if (to.matched.some((record) => record.meta.skipIfAuth)) {
+    if (store.getters.isAuthenticated) {
+      next({ path: "/builds" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
