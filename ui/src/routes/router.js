7d07b495871727a82effbd9f4447066358e74660
ultram4rine <ultramarine730@gmail.com>
2020-09-15 21:11:24 +0400

    import store in router

--- ui/src/routes/router.js
+++ ui/src/routes/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import store from "@/store/store";
+
 import Home from "@/components/Home.vue";
 import Login from "@/components/Login.vue";
 
