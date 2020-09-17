fc0e6530cb4c11f347d3bd2620562c6356d03a4f
ultram4rine <ultramarine730@gmail.com>
2020-09-17 10:54:19 +0400

    added helper for similar switches

---
+++ ui/src/helpers/useSwitches.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { ref } from "@vue/composition-api";
+
+import config from "@/config";
+
+const similarSwitchesEndpoint = `${config.apiURL}/similar`;
+
+export default function () {
+  const similarSwitches = ref([]);
+
+  const getSimilarSwitches = async (name) => {
+    try {
+      const resp = await axios.post(similarSwitchesEndpoint, {
+        name: name,
+      });
+      return resp.data;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  };
+
+  return {
+    similarSwitches,
+
+    getSimilarSwitches,
+  };
+}
