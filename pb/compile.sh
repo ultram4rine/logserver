8d8fa2b9ad2e6399a433b26dc39de02ad7ac80ff
ultram4rine <ultramarine730@gmail.com>
2020-10-25 23:14:13 +0400

    changed compile script

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-protoc -I. --go_out . --go_opt plugins=grpc --go_opt paths=source_relative log.proto
-protoc -I. --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative log.proto
-protoc -I. --openapiv2_out . --openapiv2_opt logtostderr=true log.proto
+protoc -I . --go_out . --go-grpc_out . \
+    --go_opt paths=source_relative \
+    --go-grpc_opt paths=source_relative log.proto
+protoc -I . --grpc-gateway_out . \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative log.proto
+protoc -I . --openapiv2_out . \
+    --openapiv2_opt logtostderr=true log.proto
