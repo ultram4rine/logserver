ee847fb9b5fc1f273fa3a49b5303224bf82798b1
ultram4rine <ultramarine730@gmail.com>
2020-11-27 22:32:23 +0400

    use go generate cmd instead of sh script

---
+++ pb/log.go
@@ -0,0 +1,3 @@
+package pb
+
+//go:generate protoc -I . --go_out . --go_opt paths=source_relative --go-grpc_out . --go-grpc_opt paths=source_relative --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --openapiv2_out . --openapiv2_opt logtostderr=true log.proto
